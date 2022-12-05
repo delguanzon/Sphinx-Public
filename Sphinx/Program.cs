0d017466ecd4e51963b3116701667247e13aab8a
delguanzon <yodelguanzon@gmail.com>
2022-12-05 13:36:09 -0800

    Add project files.

---
+++ Sphinx/Program.cs
@@ -0,0 +1,37 @@
+﻿using Sphinx.Riddle;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+  public static void Main()
+  {
+
+    Riddle riddle = new Riddle();
+
+    Console.WriteLine("Sphinx: Welcome! Answer my Riddle to Pass");
+
+    Console.WriteLine("Sphinx: Answer me these: " + riddle.GetRiddle());
+    string answer = Console.ReadLine();
+
+    if(riddle.checkAnswer(answer))
+    {
+      Console.WriteLine("Sphinx: Congrats! You pass!");
+      Console.WriteLine("Sphinx: Do you want another riddle?[Y][N]");
+      string ans = Console.ReadLine();
+      if(ans.ToLower() == "y")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Thank you for playing!");
+      }
+
+    }
+    else
+    {
+      Console.WriteLine("Sphinx: WRONG! *Sphinx ate you* GG");
+    }
+  }
+}
---
+++ Sphinx/Riddle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphinx.Riddle
+{
+  public class Riddle
+  {
+    private Dictionary<string, string> _riddlePairs = new Dictionary<string, string>() {
+      {"What gets wet while drying?","towel"},
+      {"What can you catch, but not throw", "cold" },
+      {"What has many teeth, but cannot bite?","comb" }
+    };
+
+    private string _currentAnswer;
+
+    public string GetRiddle()
+    {
+      return GetRandom();
+    }
+
+    private string GetRandom()
+    {
+
+      string riddle = "";
+      Random rnd = new Random();
+      int index = rnd.Next(0, _riddlePairs.Count - 1);
+
+      riddle = _riddlePairs.ElementAt(index).Key;
+      _currentAnswer = _riddlePairs.ElementAt(index).Value;
+
+      return riddle;
+    }
+
+    public bool checkAnswer(string answer)
+    {
+      bool answerFound = answer.Trim().ToLower().Contains(_currentAnswer);
+      return answerFound;
+    }
+  }
+}
